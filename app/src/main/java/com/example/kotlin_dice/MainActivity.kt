234506a0f2e84ad4f008466c9dce0e1cd9cf83b1
Lucas Aster <asterlc102@gmail.com>
2022-08-26 21:50:04 -0300

    test instance and convert it to string after using methods

--- app/src/main/java/com/example/kotlin_dice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_dice/MainActivity.kt
@@ -13,18 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.btnRoll)
-        rollButton.setOnClickListener {
-            rollDice()
-
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textRollSide)
-        resultTextView.text = diceRoll.toString()
+        resultTextView.text = Dice(6).roll().toString()
         val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
         toast.show()
     }
