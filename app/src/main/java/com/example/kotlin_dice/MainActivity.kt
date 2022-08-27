789aeee794124201121bc870ecf1bfbb93801bc4
Lucas Aster <asterlc102@gmail.com>
2022-08-26 21:31:52 -0300

    first imports

--- app/src/main/java/com/example/kotlin_dice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_dice/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.kotlin_dice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.btnRoll)
+        rollButton.setOnClickListener{
+            val resultTextView: TextView = findViewById(R.id.textRollSide)
+            resultTextView.text = "6"
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
     }
+
 
     val myFirstDice = Dice(6)
     val diceRoll = myFirstDice.roll()
