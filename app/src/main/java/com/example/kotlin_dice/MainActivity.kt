1561153c3c021a1bba1c61d2e9259f03e4f46071
Lucas Aster <asterlc102@gmail.com>
2022-08-26 21:42:42 -0300

    dice roll working

--- app/src/main/java/com/example/kotlin_dice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_dice/MainActivity.kt
@@ -13,17 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.btnRoll)
-        rollButton.setOnClickListener{
-            val resultTextView: TextView = findViewById(R.id.textRollSide)
-            resultTextView.text = "6"
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+        rollButton.setOnClickListener {
+            rollDice()
 
         }
     }
 
+    private fun rollDice() {
 
-    val myFirstDice = Dice(6)
-    val diceRoll = myFirstDice.roll()
-
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textRollSide)
+        resultTextView.text = diceRoll.toString()
+        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
 }
